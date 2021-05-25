4a8b406272d83c9723c55a5cb190f53cda8508ac
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-05-25 11:28:10 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,10 +1,10 @@
 resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "172.2.0.0/16"
+  vpc_id     = "vpc-08d01e6ccd0e44429"
+  cidr_block = "10.0.0.1/16"
 }
 resource "aws_subnet" "in_secondary_cidr" {
   vpc_id     = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
-  cidr_block = "172.2.0.0/24"
+  cidr_block = "10.0.0.1/16"
   tags = {
     Name = "demo-subnet"
     Purpose = "Jenkins Demo"
