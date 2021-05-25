e972639cda49f8198924ad71feb99c848bba4abf
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-05-25 11:22:45 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "in_secondary_cidr" {
   vpc_id     = "vpc-08d01e6ccd0e44429"
-  cidr_block = "172.2.0.0/24"
+  cidr_block = "10.0.0.1/16"
   tags = {
     Name = "demo-subnet"
     Purpose = "Jenkins Demo"
